e6ad7513239c84b24786d1c10d8eea1388b15e28
huzy_1848 <huzy@bjaisi.com>
2023-07-09 07:00:45 +0000

    update dptb/dataprocess/datareader.py. In data loader, we add load_data_mol and read_data_mol. The only difference is that, in molecules, we only care about the structures and the eigenvalues, but not the kpoints or bandinfo, wannier sets, which are excluded in the code.
    Signed-off-by: huzy_1848 <huzy@bjaisi.com>

--- dptb/dataprocess/datareader.py
+++ dptb/dataprocess/datareader.py
@@ -9,6 +9,45 @@ from dptb.dataprocess.processor import Processor
 from dptb.utils.tools import j_loader
 from dptb.utils.argcheck import normalize_bandinfo
 
+def read_data_mol(path, cutoff, proj_atom_anglr_m, proj_atom_neles, \
+    onsitemode:str='uniform', time_symm=True, **kwargs):
+    batch_size = kwargs['train']['batch_size']
+    struct_list_sets, eigens_sets = [], []
+    data_dirs = glob.glob(path + "/*")
+    struct_list, eigens = [], []
+    for ii in range(len(data_dirs)):
+        with open(data_dirs[ii], 'rb') as f:
+            loaded_dict = pickle.load(f)
+        iatom = Atoms(loaded_dict["Name"], positions=loaded_dict["positions"])
+        eigs = loaded_dict["eigvals"].reshape(1, -1)
+        struct = BaseStruct(atom=iatom, format='ase', cutoff=cutoff, proj_atom_anglr_m=proj_atom_anglr_m, proj_atom_neles=proj_atom_neles, onsitemode=onsitemode, time_symm=time_symm)
+        
+        eigens.append(eigs)
+        struct_list.append(struct)
+
+        if ii % batch_size == batch_size - 1 or ii == len(data_dirs) - 1:
+            eigens_sets.append(eigens)
+            struct_list_sets.append(struct_list)
+            eigens, struct_list = [], []
+    return struct_list_sets, eigens_sets
+
+def get_data_mol(path, batch_size, bond_cutoff, env_cutoff, onsite_cutoff, \
+        proj_atom_anglr_m, proj_atom_neles, sorted_onsite="st", sorted_bond="st", sorted_env="st", \
+        onsitemode:str='uniform', time_symm=True, device='cpu', dtype=torch.float32, if_shuffle=True, **kwargs):
+    struct_list_sets, eigens_sets = read_data_mol(path, bond_cutoff, \
+        proj_atom_anglr_m, proj_atom_neles, onsitemode, time_symm, **kwargs)
+    assert len(struct_list_sets) == len(eigens_sets)
+    processor_list = []
+    for i in range(len(struct_list_sets)):
+        processor_list.append(
+            Processor(structure_list=struct_list_sets[i], batchsize=batch_size,
+                        eigen_list=eigens_sets[i], device=device, 
+                        dtype=dtype, env_cutoff=env_cutoff, onsite_cutoff=onsite_cutoff, onsitemode=onsitemode, 
+                        sorted_onsite=sorted_onsite, sorted_bond=sorted_bond, sorted_env=sorted_env, 
+                        if_shuffle = if_shuffle))
+    return processor_list
+   
+
 def read_data(path, prefix, cutoff, proj_atom_anglr_m, proj_atom_neles, onsitemode:str='uniform', time_symm=True, **kwargs):
     """根据文件路径和prefix的读取文件夹下的数据文件,并存储为神经网络模型的输入格式数据
     """
